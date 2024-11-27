5d387c6e4b835c5d2ae3b471f51939965350952b
Nada Ali <nadaaliwazeer@gmail.com>
2024-11-27 23:52:27 +0200

    Database

--- dataBase/dbConnection.js
+++ dataBase/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 
 export const dbConnection = () => {
-    mongoose.connect('mongodb://127.0.0.1:27017/noon')
+    mongoose.connect('mongodb+srv://Nadaali:Nadaali@cluster0.9staiq5.mongodb.net/ecommerce')
     .then( () => {
         console.log('Database connected successfully')
     })
@@ -10,3 +10,5 @@ export const dbConnection = () => {
         console.log('Database connection failed',err)
     })
 }
+//'mongodb+srv://Nadaali:Nadaali@cluster0.9staiq5.mongodb.net/ecommerce'
+//mongodb://127.0.0.1:27017/noon'
